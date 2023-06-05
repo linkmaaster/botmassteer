b8605f750f938f494f9cb618fa3299daf7a5aab9
linkmaaster <135422172+linkmaaster@users.noreply.github.com>
2023-06-05 14:14:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 26184804
+API_HASH     = "ffd7031b70e80e84d48043db9addc73d"
+BOT_TOKEN    = "6246376417:AAEUyaiaptWw32G2K9nLLd4yOy20JYb75Rs"
+SESSION      = "BQGPjGQAOaNYX_v4xOMeXJrkvWVhzhsMv1kOxXytESUiaDmjcWRS7izvhedx9-VeycdW4-JCBHFZ_3FApWrsJvfs4VK0ZPT5YWwQtJt1_QeXCRpfy3uJW6i_xjdzTXiKiMKoNTQ3Etx-AAcu0dmYE4OomI-DMoOmzAEm6RpM-kCIN2xEkuyqY5rRcBw6ui8n4HlfWjZKPjpU3G3N4oYkxzKQpcYY9OpzbF1_ieLaDs1nbOv1HDagv96FZze5-b9cLfFB7qZXFirjnCVdDdbf9KQY_xbkPIK0JbGmrawjylqOcbaiC3h7dk6MoGlkzxpm4wjLnde8VR_vUrOaAkgb9mFmV-jqngAAAAF0UCPhAQ"
+DATABASE_URI = "mongodb+srv://botdevelop:<password>@cluster0.30auzkg.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001942311037
+ADMIN        = 5053078995
+CHANNEL      = "@link_stoorage"
